bda0454f59a2b98b6ced3c57aa48e64cc4586bef
sonthh <tranhuuhongson@gmail.com>
2021-08-21 13:59:45 +0700

    Updates

--- static/js/main.bf27440d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonplinhvo=this.webpackJsonplinhvo||[]).push([[0],{10:function(n,t,e){"use strict";e.r(t);var i=e(3),o=e.n(i),c=e(0),h=function(){return Object(c.jsx)("h1",{children:"Vo Thi Ai Linh"})};e(9);o.a.render(Object(c.jsx)(h,{}),document.getElementById("root"))},9:function(n,t,e){}},[[10,1,2]]]);
-//# sourceMappingURL=main.bf27440d.chunk.js.map
