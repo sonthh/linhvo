f9e08abb0cdd57d0126fca45c577bc1d7e90806b
sonthh <tranhuuhongson@gmail.com>
2021-08-21 14:22:41 +0700

    Updates

--- static/js/main.e626e841.chunk.js
+++ static/js/main.e626e841.chunk.js
@@ -1,2 +1,2 @@
 (this.webpackJsonplinhvo=this.webpackJsonplinhvo||[]).push([[0],{27:function(n,t,e){},28:function(n,t,e){"use strict";e.r(t);var o=e(15),c=e.n(o),r=e(1),h=e(6),i=[{path:"/",component:function(){return Object(h.jsx)("h1",{children:"HomePage"})}},{path:"/about",component:function(){return Object(h.jsx)("h1",{children:"About"})}}],u=function(){return Object(h.jsx)(r.c,{children:i.map((function(n,t){return Object(h.jsx)(r.a,{path:"/linhvo"+n.path,component:n.component,exact:!0},t)}))})},a=(e(27),e(11));console.log("/linhvo"),c.a.render(Object(h.jsx)(a.a,{children:Object(h.jsx)(u,{})}),document.getElementById("root"))}},[[28,1,2]]]);
-//# sourceMappingURL=main.33baa51b.chunk.js.map
+//# sourceMappingURL=main.e626e841.chunk.js.map
